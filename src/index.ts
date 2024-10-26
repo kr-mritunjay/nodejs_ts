7a2e0333cb0bf4243b82136c60a39381feb64d15
Mritunjay kumar <mritunjaykr160@gmail.com>
2024-10-26 10:06:18 +0530

    get query implimentation

--- src/index.ts
+++ src/index.ts
@@ -14,21 +14,21 @@ client
 
 // export default client;
 
-// async function createUsersTable() {
-//   await client.connect();
-//   const result = await client.query(`
-//         CREATE TABLE users (
-//             id SERIAL PRIMARY KEY,
-//             username VARCHAR(50) UNIQUE NOT NULL,
-//             email VARCHAR(50) UNIQUE NOT NULL,
-//             password VARCHAR(255) NOT NULL,
-//             create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-//         );
-//     `);
-//   console.log(result);
-// }
+async function createUsersTable() {
+  // await client.connect();
+  const result = await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(50) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    `);
+  console.log(result);
+}
 
-// createUsersTable();
+createUsersTable();
 
 // other way of using insertion in sql directly values are udated too db
 
